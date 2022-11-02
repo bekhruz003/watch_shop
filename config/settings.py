d8fc77165114007b4b5b21a25f8b72e7f772a9f7
Behruz <behruzparpiyev20@gmail.com>
2022-11-03 02:13:18 +0500

    added translation

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ DEBUG = config('DEBUG')
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -90,6 +92,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('ru', 'Russian'),
+    ('uz', 'Uzbek'),
+)
+
+MODELTRANSLATION_DEFAULT_LANGUAGES = 'en-us'
+
+LOCALE_PATHS = BASE_DIR / 'locale',
 
 TIME_ZONE = 'UTC'
 
--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from .models import ProductModel
+from modeltranslation.admin import TranslationAdmin
 
 
 @admin.register(ProductModel)
-class ProductModelAdmin(admin.ModelAdmin):
+class ProductModelAdmin(TranslationAdmin):
     list_display = ['title', 'price', 'real_price', 'discount', 'created_at']
     list_display_links = ['title', 'price', 'created_at']
     list_filter = ['created_at']
     search_fields = ['title', 'price']
     readonly_fields = ['real_price']
+
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
